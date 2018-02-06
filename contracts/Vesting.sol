f9cd729577a4f0425811ab1dcbea239f5350ed6c
Martin Grigorov <Air@Martin.local>
2018-02-06 23:46:33 +0200

    Fix unit tests

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -4,7 +4,6 @@ import '../node_modules/zeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import './VestibleToken.sol';
 
 contract Vesting is Ownable {
-	address public owner;
 	address public tokenAddress;
 	uint256 public startDate;
 	uint256 public tokenBalance;
@@ -34,17 +33,17 @@ contract Vesting is Ownable {
 
 		if (tokenBalance == 0) {
 			tokenBalance = token.balanceOf(this);
-			uint256 owedFirstPeriodTokens = tokenBalance * 20 / 100;
-			uint256 owedSecondPeriodTokens = tokenBalance * 40 / 100;
-			uint256 owedThirdPeriodTokens = tokenBalance * 60 / 100;
-			uint256 owedFourthPeriodTokens = tokenBalance * 80 / 100;
-			uint256	owedFifthPeriodTokens = tokenBalance * 100 / 100;
+			uint256 owedFirstPeriodTokens = (tokenBalance * 20) / 100;
+			uint256 owedSecondPeriodTokens = (tokenBalance * 40) / 100;
+			uint256 owedThirdPeriodTokens = (tokenBalance * 60) / 100;
+			uint256 owedFourthPeriodTokens = (tokenBalance * 80) / 100;
+			uint256	owedFifthPeriodTokens = (tokenBalance * 100) / 100;
 			uint256 claimedTokens = 0;
 		}
 		if (now < startDate + firstPeriod) {
 			require(owedFirstPeriodTokens > claimedTokens);
-			token.transfer(owner, owedFirstPeriodTokens - claimedTokens);
-			LogTransferSuccessful(owner, owedFirstPeriodTokens - claimedTokens);
+			token.transfer(owner, (owedFirstPeriodTokens - claimedTokens));
+			LogTransferSuccessful(owner, (owedFirstPeriodTokens - claimedTokens));
 			claimedTokens = owedFirstPeriodTokens;
 			return;
 		}
--- test/vesting.js
+++ test/vesting.js
@@ -40,135 +40,143 @@ contract('Vesting', function (accounts) {
 
 	it("should be owned by owner", async function () {
 		let _owner = await vestingContract.owner({
-			from: vestingContract.address
+			from: contractOwnerAddress
 		});
 		assert.strictEqual(_owner, contractOwnerAddress, "contract is not owned by owner");
 	});
 
-	// // Claim function tests
+	// Claim function tests
 	it("should transfer the tokens to the owner for the first period", async function () {
 		let initialOwnerBalance = await token.balanceOf(vestingContract.address);
 		await timeTravel(web3, day);
 		await vestingContract.claim({
 			from: contractOwnerAddress
 		});
-
 		let finalOwnerBalance = await token.balanceOf(vestingContract.address);
-		console.log(finalOwnerBalance);
 		assert.equal(finalOwnerBalance.toString(), initialOwnerBalance.toString() - 200, "The transfer wasn't successful")
 	})
 
-	// it("should transfer the tokens to the owner for the second period", async function () {
-	// 	let initialOwnerBalance = web3.eth.getBalance(contractOwnerAddress).toNumber();
-	// 	await timeTravel(web3, firstPeriod + day);
-	// 	await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.equal(web3.eth.getBalance(contractOwnerAddress).toNumber(), initialOwnerBalance + 200, "The transfer wasn't successful")
-	// })
+	it("should transfer the tokens to the owner for the second period", async function () {
+		let initialOwnerBalance = await token.balanceOf(vestingContract.address);
+		await timeTravel(web3, firstPeriod + day);
+		await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		let finalOwnerBalance = await token.balanceOf(vestingContract.address);
+		assert.equal(finalOwnerBalance.toString(), initialOwnerBalance.toString() - 400, "The transfer wasn't successful")
+	})
 
-	// it("should transfer the tokens to the owner for the third period", async function () {
-	// 	let initialOwnerBalance = web3.eth.getBalance(contractOwnerAddress).toNumber();
-	// 	await timeTravel(web3, secondPeriod + day);
-	// 	await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.equal(web3.eth.getBalance(contractOwnerAddress).toNumber(), initialOwnerBalance + 200, "The transfer wasn't successful")
-	// })
+	it("should transfer the tokens to the owner for the third period", async function () {
+		let initialOwnerBalance = await token.balanceOf(vestingContract.address);
+		await timeTravel(web3, secondPeriod + day);
+		await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		let finalOwnerBalance = await token.balanceOf(vestingContract.address);
+		assert.equal(finalOwnerBalance.toString(), initialOwnerBalance.toString() - 600, "The transfer wasn't successful")
+	})
 
-	// it("should transfer the tokens to the owner for the fourth period", async function () {
-	// 	let initialOwnerBalance = web3.eth.getBalance(contractOwnerAddress).toNumber();
-	// 	await timeTravel(web3, thirdPeriod + day);
-	// 	await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.equal(web3.eth.getBalance(contractOwnerAddress).toNumber(), initialOwnerBalance + 200, "The transfer wasn't successful")
-	// })
+	it("should transfer the tokens to the owner for the fourth period", async function () {
+		let initialOwnerBalance = await token.balanceOf(vestingContract.address);
+		await timeTravel(web3, thirdPeriod + day);
+		await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		let finalOwnerBalance = await token.balanceOf(vestingContract.address);
+		assert.equal(finalOwnerBalance.toString(), initialOwnerBalance.toString() - 800, "The transfer wasn't successful")
+	})
 
-	// it("should transfer the tokens to the owner for the fifth period", async function () {
-	// 	let initialOwnerBalance = web3.eth.getBalance(contractOwnerAddress).toNumber();
-	// 	await timeTravel(web3, fourthPeriod + day);
-	// 	await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.equal(web3.eth.getBalance(contractOwnerAddress).toNumber(), initialOwnerBalance + 200, "The transfer wasn't successful")
-	// })
+	it("should transfer the tokens to the owner for the fifth period", async function () {
+		await timeTravel(web3, fourthPeriod + day);
+		await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		let finalOwnerBalance = await token.balanceOf(vestingContract.address);
+		assert.equal(finalOwnerBalance.toString(), 0, "The transfer wasn't successful")
+	})
 
-	// it("should transfer the tokens to the owner for the end of the period", async function () {
-	// 	let initialOwnerBalance = web3.eth.getBalance(contractOwnerAddress).toNumber();
-	// 	await timeTravel(web3, fifthPeriod + day);
-	// 	await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.equal(web3.eth.getBalance(contractOwnerAddress).toNumber(), initialOwnerBalance + 1000, "The transfer wasn't successful")
-	// })
+	it("should transfer the tokens to the owner for the end of the period", async function () {
+		await timeTravel(web3, fifthPeriod + day);
+		await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		let finalOwnerBalance = await token.balanceOf(vestingContract.address);
+		assert.equal(finalOwnerBalance.toString(), 0, "The transfer wasn't successful")
+	})
 
-	// //Event Tests
-	// it("should emit one event if transfer for the first period is successful", async function () {
-	// 	const expectedEvent = 'LogTransferSuccessful'
-	// 	await timeTravel(web3, day);
-	// 	let result = await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
-	// 	assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
-	// })
+	//Event Tests
+	it("should emit one event if transfer for the first period is successful", async function () {
+		const expectedEvent = 'LogTransferSuccessful'
+		await timeTravel(web3, day);
+		let result = await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
+		assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
+	})
 
-	// it("should emit one event if transfer for the second period is successful", async function () {
-	// 	const expectedEvent = 'LogTransferSuccessful'
-	// 	await timeTravel(web3, firstPeriod + day);
-	// 	let result = await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
-	// 	assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
-	// })
-	// it("should emit one event if transfer for the third period is successful", async function () {
-	// 	const expectedEvent = 'LogTransferSuccessful'
-	// 	await timeTravel(web3, secondPeriod + day);
-	// 	let result = await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
-	// 	assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
-	// })
+	it("should emit one event if transfer for the second period is successful", async function () {
+		const expectedEvent = 'LogTransferSuccessful'
+		await timeTravel(web3, firstPeriod + day);
+		let result = await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
+		assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
+	})
+	it("should emit one event if transfer for the third period is successful", async function () {
+		const expectedEvent = 'LogTransferSuccessful'
+		await timeTravel(web3, secondPeriod + day);
+		let result = await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
+		assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
+	})
 
-	// it("should emit one event if transfer for the fourth period is successful", async function () {
-	// 	const expectedEvent = 'LogTransferSuccessful'
-	// 	await timeTravel(web3, thirdPeriod + day);
-	// 	let result = await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
-	// 	assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
-	// })
+	it("should emit one event if transfer for the fourth period is successful", async function () {
+		const expectedEvent = 'LogTransferSuccessful'
+		await timeTravel(web3, thirdPeriod + day);
+		let result = await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
+		assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
+	})
 
-	// it("should emit one event if transfer for the fifth period is successful", async function () {
-	// 	const expectedEvent = 'LogTransferSuccessful'
-	// 	await timeTravel(web3, fourthPeriod + day);
-	// 	let result = await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
-	// 	assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
-	// })
+	it("should emit one event if transfer for the fifth period is successful", async function () {
+		const expectedEvent = 'LogTransferSuccessful'
+		await timeTravel(web3, fourthPeriod + day);
+		let result = await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
+		assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
+	})
 
-	// it("should emit one event if transfer for beyond the fifth period is successful", async function () {
-	// 	const expectedEvent = 'LogTransferSuccessful'
-	// 	await timeTravel(web3, fifthPeriod + day);
-	// 	let result = await vestingContract.claim({
-	// 		from: contractOwnerAddress
-	// 	});
-	// 	assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
-	// 	assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
-	// })
+	it("should emit one event if transfer for beyond the fifth period is successful", async function () {
+		const expectedEvent = 'LogTransferSuccessful'
+		await timeTravel(web3, fifthPeriod + day);
+		let result = await vestingContract.claim({
+			from: contractOwnerAddress
+		});
+		assert.lengthOf(result.logs, 1, "There should be 1 event emitted from claiming the tokens !");
+		assert.strictEqual(result.logs[0].event, expectedEvent, `The event emitted was ${result.logs[0].event} instead of ${expectedEvent}`);
+	})
 
-	// //Negative case 
-	// it("shuld throw if the token balance is not greater that zero", async function () {
+	//Negative case 
+	// it("should throw if the token balance is not greater that zero", async function () {
 	// 	await timeTravel(web3, day);
 	// 	await expectThrow(vestingContract.claim({
 	// 		from: contractOwnerAddress
 	// 	}));
 	// });
+
+	it("should throw if the claim is not called from the owner of the contract", async function () {
+		await timeTravel(web3, day);
+		await expectThrow(vestingContract.claim({
+			from: accounts[3]
+		}));
+	});
 
 })
